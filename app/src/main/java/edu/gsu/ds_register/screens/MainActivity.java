8b9a0c368dcf5d94ddd59c655508d16106fb095e
Joshua King <joshua@antechdevelopment.com>
2016-11-23 14:13:31 -0500

    Answered Chris's questions and removed obsolete controllers.

--- app/src/main/java/edu/gsu/ds_register/screens/MainActivity.java
+++ app/src/main/java/edu/gsu/ds_register/screens/MainActivity.java
@@ -24,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 		/*
-		Am I on the right track?
+		//	Am I on the right track? - Sort of, except the id's you use (R.id.<THE_ID>) should be identical and unique to the id in the XML
+		//	See here: https://developer.android.com/reference/android/view/View.html
 
 		final EditText getFirstName = (EditText) findViewById(R.id.getFirstName);
         final EditText getLastName = (EditText) findViewById(R.id.getLastName);
@@ -34,9 +35,10 @@ public class MainActivity extends AppCompatActivity {
         final EditText getPhoneNumber = (Button) findViewById(R.id.getPhoneNumber);
         final EditText getDob = (Button) findViewById(R.id.getDob);
 
-        //if it's a button then  final Button getButton = (Button) findViewById(R.id.getButton);
+        //if it's a button then  final Button getButton = (Button) findViewById(R.id.getButton); // - Not really, it should be like the following line
+		//final Button getButton = (Button) findViewById(R.id.email_sign_in_button); // - now the "final" part is optional in this case because we don't use it in an anonymous class
 
-		then the button would have it's own button.setOnClickListener()
+		then the button would have it's own button.setOnClickListener() - Yes
 		 */
 
 
