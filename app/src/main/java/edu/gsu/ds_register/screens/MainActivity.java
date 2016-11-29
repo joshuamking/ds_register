68ab4776cfe7e485fd1d0f25e001dc0cafa21349
Joshua King <joshua@antechdevelopment.com>
2016-11-28 22:59:19 -0500

    Added visualization screen

--- app/src/main/java/edu/gsu/ds_register/screens/MainActivity.java
+++ app/src/main/java/edu/gsu/ds_register/screens/MainActivity.java
@@ -2,14 +2,23 @@ package edu.gsu.ds_register.screens;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import edu.gsu.ds_register.R;
 import edu.gsu.ds_register.listener.SimpleListValueListener;
 import edu.gsu.ds_register.model.PersonModel;
 import edu.gsu.ds_register.utils.FirebaseUtils;
+import edu.gsu.ds_register.utils.StringUtils;
 
 import java.util.ArrayList;
 
@@ -18,10 +27,50 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		final TextView signUpButton = (TextView) findViewById(R.id.signin_signup_button);
+		final TextView signUpButton     = (TextView) findViewById(R.id.signin_signup_button);
+		final TextView signInButton     = (TextView) findViewById(R.id.signin_signin_button);
+		final EditText emailEditText    = (EditText) findViewById(R.id.signin_email_field);
+		final EditText passwordEditText = (EditText) findViewById(R.id.signin_password_field);
+
+		FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+		if (currentUser != null) {
+			openVisualization();
+			Toast.makeText(this, String.format("Signed in as %s", currentUser.getEmail()), Toast.LENGTH_LONG).show();
+		}
+
 		signUpButton.setOnClickListener(new View.OnClickListener() {
 			@Override public void onClick (View v) {
 				startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+			}
+		});
+		signInButton.setOnClickListener(new View.OnClickListener() {
+			@Override public void onClick (final View v) {
+				final String emailValue    = String.valueOf(emailEditText.getText());
+				String       passwordValue = String.valueOf(passwordEditText.getText());
+
+				if (StringUtils.isEmptyOrNull(emailValue) || StringUtils.isEmptyOrNull(passwordValue)) {
+					Snackbar.make(v, "Please fill out all of the fields", Snackbar.LENGTH_INDEFINITE).show();
+					return;
+				}
+
+				FirebaseAuth.getInstance()
+							.signInWithEmailAndPassword(emailValue, passwordValue)
+							.addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+								@Override public void onComplete (@NonNull Task<AuthResult> task) {
+									if (task.isSuccessful()) {
+										openVisualization();
+									}
+									else {
+										Snackbar.make(v, "An error occurred while signing in. Please verify your credentials.", Snackbar.LENGTH_INDEFINITE)
+												.setAction("Forgot Password", new View.OnClickListener() {
+													@Override public void onClick (View v) {
+														FirebaseAuth.getInstance().sendPasswordResetEmail(emailValue);
+													}
+												})
+												.show();
+									}
+								}
+							});
 			}
 		});
 
@@ -31,4 +80,6 @@ public class MainActivity extends AppCompatActivity {
 			}
 		});
 	}
+
+	private void openVisualization () {startActivity(new Intent(MainActivity.this, VisualizationActivity.class));}
 }
---
+++ app/src/main/java/edu/gsu/ds_register/screens/VisualizationActivity.java
@@ -0,0 +1,61 @@
+package edu.gsu.ds_register.screens;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import edu.gsu.ds_register.R;
+import edu.gsu.ds_register.listener.SimpleListValueListener;
+import edu.gsu.ds_register.model.PersonModel;
+import edu.gsu.ds_register.utils.FirebaseUtils;
+
+import java.util.ArrayList;
+
+public class VisualizationActivity extends AppCompatActivity {
+	@Override protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_visualization);
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
+		ListView                     participantsListView = (ListView) findViewById(R.id.visualize_list_view);
+		final ArrayList<PersonModel> persons              = new ArrayList<>();
+		final ArrayAdapter<PersonModel> personModelArrayAdapter = new ArrayAdapter<PersonModel>(this, R.layout.person_info, persons) {
+			@NonNull @Override public View getView (int position, View convertView, ViewGroup parent) {
+				View view;
+				if (convertView == null) {
+					view = View.inflate(getContext(), R.layout.person_info, null);
+				}
+				else {
+					view = convertView;
+				}
+
+				TextView nameView  = (TextView) view.findViewById(R.id.person_info_name_label);
+				TextView emailView = (TextView) view.findViewById(R.id.person_info_email_label);
+				TextView phoneView = (TextView) view.findViewById(R.id.person_info_phone_label);
+
+				PersonModel person = persons.get(position);
+
+				nameView.setText(person.getName());
+				emailView.setText(person.getEmail());
+				phoneView.setText(person.getPhoneNumber());
+
+				return view;
+			}
+		};
+		participantsListView.setAdapter(personModelArrayAdapter);
+
+		FirebaseUtils.addValueListener("persons", new SimpleListValueListener<PersonModel>(PersonModel.class) {
+			@Override public void onValueRetrieved (ArrayList<PersonModel> personModels) {
+				persons.clear();
+				persons.addAll(personModels);
+				personModelArrayAdapter.notifyDataSetChanged();
+			}
+		});
+	}
+}
